bc6290ee66a2fd466ac72d66e80ea5dcf43a5fbf
reena <reena.sanjay.sharma@gmail.com>
2018-01-26 22:07:21 +0000

    can't sign up with blank password or password confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User
 
   property :id, Serial
   property :email, String, format: :email_address, required: true
-  property :password_digest, Text
+  property :password_digest, Text, required: true
 
   validates_confirmation_of :password
   
